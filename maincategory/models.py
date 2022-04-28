c5237d6f0b7fde75b0c7a0f098ffcee366c791e6
holawan <asdf134652@gmail.com>
2022-04-28 23:38:10 +0900

    [add] migrations test

--- maincategory/models.py
+++ maincategory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class RECIPE_BASIC(models.Model) :
+    #레시피 이름 
+    recipeName = models.CharField(max_length = 30)
+    #설명 
+    summary = models.TextField()
+    #국가 분류 (?) 한식/양식/중식 
+    nationName = models.CharField(max_length= 30)
+    #요리 분류 (밥/떡/국/만두/면 등 )
+    typeName = models.CharField(max_length = 30)
+    #조리시간 
+    cookingTime = models.IntegerField()
+    #칼로리 
+    calorie = models.IntegerField()
+    #인분 
+    QNT = models.IntegerField()
+    #조리 레벨 
+    level = models.CharField(max_length=10)
+    #재료 종류 ? 
+    idrType = models.CharField(max_length = 20,null=True)
+    #이미지
+    image = models.TextField()
